65bf4c7d814d1cfe2119789a5dac3a10a949e3a6
Leandro Bernsmueller <lebernsmuller@hotmail.com>
2021-06-18 19:17:32 +0200

    added edit title

--- src/components/tasks/taskEditor/TaskEditor.tsx
+++ src/components/tasks/taskEditor/TaskEditor.tsx
@@ -1,17 +1,36 @@
 import * as React from "react";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
+import { TextField } from "office-ui-fabric-react";
 
 import { ITaskEditorProps } from "./ITaskEditorProps";
+import { ITask } from "../../../entities/ITask";
 
 const TaskEditor: FunctionComponent<ITaskEditorProps> = (props) => {
+
+  const [changedTask, setChangedTask] = useState({} as ITask);
+
+  useEffect(() => {
+    const task = props.task ?? {} as ITask;
+    setChangedTask(task);
+  }, [props.task]);
 
   if (props.task === null) {
     return null;
   }
 
   return (
-    <div>Edit Form for {JSON.stringify(props.task)} ... to be implemented</div>
+    <div>
+      <TextField label={'Task'} value={changedTask.title} onChange={event => onChangeTask((event.target as any).value, 'title')} />
+    </div>
   );
-}
+
+  function onChangeTask(value: string, propertyName: keyof ITask): void {
+    const change: ITask = {
+      ...changedTask,
+      [propertyName]: value
+    };
+    setChangedTask(change);
+  }
+};
 
 export default TaskEditor;
