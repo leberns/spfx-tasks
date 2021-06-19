9d8c70a42eb95cc0c3012fa7d395bc78dda4a7d9
Leandro Bernsmueller <lebernsmuller@hotmail.com>
2021-06-19 21:35:02 +0200

    added status and date edit

--- src/components/tasks/taskEditor/TaskEditor.tsx
+++ src/components/tasks/taskEditor/TaskEditor.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
 import { FunctionComponent, useState, useEffect } from "react";
-import { TextField } from "office-ui-fabric-react";
+import { DatePicker, Dropdown, IDropdownOption, TextField } from "office-ui-fabric-react";
 
 import { ITaskEditorProps } from "./ITaskEditorProps";
 import { ITask } from "../../../entities/ITask";
+import { TaskStatus } from "../../../enums/TaskStatus";
 
 const TaskEditor: FunctionComponent<ITaskEditorProps> = (props) => {
 
   const [changedTask, setChangedTask] = useState({} as ITask);
+  const [options] = useState(() => {
+    const initialOptions: IDropdownOption[] = [];
+    for (const status in TaskStatus) {
+      initialOptions.push({ key: status, text: status });
+    }
+    return initialOptions;
+  });
 
   useEffect(() => {
     const task = props.task ?? {} as ITask;
@@ -20,11 +28,13 @@ const TaskEditor: FunctionComponent<ITaskEditorProps> = (props) => {
 
   return (
     <div>
-      <TextField label={'Task'} value={changedTask.title} onChange={event => onChangeTask((event.target as any).value, 'title')} />
+      <TextField label='Task' value={changedTask.title} onChange={event => onChangeTask((event.target as any).value, 'title')} />
+      <Dropdown label='Status' options={options} selectedKey={changedTask.status} onChange={(event, item) => onChangeTask(item.text, 'status')} />
+      <DatePicker label='Due Date' value={changedTask.dueDate} onSelectDate={date => onChangeTask(date, 'dueDate')}></DatePicker>
     </div>
   );
 
-  function onChangeTask(value: string, propertyName: keyof ITask): void {
+  function onChangeTask(value: string | Date, propertyName: keyof ITask): void {
     const change: ITask = {
       ...changedTask,
       [propertyName]: value
