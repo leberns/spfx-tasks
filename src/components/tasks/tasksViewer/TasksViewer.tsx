4db0d257f8cebe22d35dc4ed2aa2efd3abe625d0
Leandro Bernsmueller <lebernsmuller@hotmail.com>
2021-06-22 21:28:58 +0200

    fix date displaying in list view

--- src/components/tasks/tasksViewer/TasksViewer.tsx
+++ src/components/tasks/tasksViewer/TasksViewer.tsx
@@ -1,39 +1,45 @@
 import * as React from 'react';
 import { FunctionComponent } from 'react';
-import { ListView, IViewField, SelectionMode, GroupOrder, IGrouping } from "@pnp/spfx-controls-react/lib/ListView";
+import { ListView, IViewField, SelectionMode } from "@pnp/spfx-controls-react/lib/ListView";
 
 import { ITasksViewerProps } from './ITasksViewerProps';
 import { ITask } from '../../../entities/ITask';
-import { DefaultButton } from 'office-ui-fabric-react';
-
-const viewFields: IViewField[] = [
-  {
-    name: 'title',
-    displayName: 'Title',
-    isResizable: true,
-    sorting: true
-  },
-  {
-    name: 'status',
-    displayName: 'Status',
-    isResizable: true,
-    sorting: true
-  },
-  {
-    name: 'dueDate',
-    displayName: 'Due Date',
-    isResizable: true,
-    sorting: true
-  },
-  {
-    name: 'assignedUser',
-    displayName: 'Assigned To',
-    isResizable: true,
-    sorting: true
-  },
-];
 
 const TasksViewer: FunctionComponent<ITasksViewerProps> = (props) => {
+
+  const viewFields: IViewField[] = [
+    {
+      name: 'title',
+      displayName: 'Title',
+      isResizable: true,
+      sorting: true,
+      minWidth: 100,
+      maxWidth: 200
+    },
+    {
+      name: 'status',
+      displayName: 'Status',
+      isResizable: true,
+      sorting: true,
+      minWidth: 100,
+    },
+    {
+      name: 'dueDate',
+      displayName: 'Due Date',
+      isResizable: true,
+      sorting: true,
+      minWidth: 100,
+      render: onRenderDueDate,
+    },
+    {
+      name: 'assignedUser',
+      displayName: 'Assigned To',
+      isResizable: true,
+      sorting: true,
+      minWidth: 100,
+    },
+  ];
+
   return (
     <div>
       <ListView
@@ -52,6 +58,12 @@ const TasksViewer: FunctionComponent<ITasksViewerProps> = (props) => {
   function onSelectedTaskChanged(tasks: ITask[]): void {
     const task = tasks.length > 0 ? tasks[0] : null;
     props.onSelectedTaskChanged(task);
+  }
+
+  function onRenderDueDate(item: any): JSX.Element {
+    const task = props.tasks.filter(t => t.id === item.id)[0];
+    const date = task.dueDate?.toLocaleDateString();
+    return <span>{date}</span>;
   }
 };
 
