d3381eb3222ec6f827b282e83da2d4cc591ad8a7
Leandro Bernsmueller <lebernsmuller@hotmail.com>
2021-07-28 18:06:58 +0200

    enable site collection app catalogs for dev and test

---
+++ scripts/Enable-AppCatalogSiteCollection.ps1
@@ -0,0 +1,7 @@
+﻿# https://docs.microsoft.com/en-us/sharepoint/dev/general-development/site-collection-app-catalog
+
+Connect-PnPOnline https://adessoleandrobernsmueller.sharepoint.com/ -Credentials (Get-Credential)
+
+Add-PnPSiteCollectionAppCatalog -site https://adessoleandrobernsmueller.sharepoint.com/sites/tasks-dev
+
+Add-PnPSiteCollectionAppCatalog -site https://adessoleandrobernsmueller.sharepoint.com/sites/tasks-test
--- scripts/Register-PnPApp.ps1
+++ scripts/Register-PnPApp.ps1
@@ -1,6 +1,7 @@
-﻿
+﻿# https://coreyroth.com/2021/01/20/how-to-use-pnp-powershell-to-deploy-sharepoint-apps-with-azure-devops/
+
 # consent to the required permissions for PnP PowerShell
 Register-PnPManagementShellAccess
 
-# register SPFxAzureDevOps app in Azure, allow to give consent and generate certificates
-Register-PnPAzureADApp -ApplicationName SPFxAzureDevOps -Tenant adessoleandrobernsmueller.onmicrosoft.com -OutPath . -DeviceLogin
+# register PnPAzureDevOps app in Azure, allow developer user to give consent and generate certificates
+Register-PnPAzureADApp -ApplicationName PnPAzureDevOps -Tenant adessoleandrobernsmueller.onmicrosoft.com -OutPath .\certificates -DeviceLogin
